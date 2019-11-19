54128ee96bd24bace1eb7b0dfbf208eafd1c5b27
ecl5pse <fantamas20@gmail.com>
2019-11-19 18:14:15 +0200

    стрелочные функци

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 'use strict';
+
+
+//
+// function f() {
+//
+//     console.log("fdhghdf");
+//
+// }
+//
+// const f2 = function () {
+//     console.log('gfdjhgdfjhgjfd')
+//
+// };
+// console.log(f2);
+
+
+/*
+const sum = function sum(a,b) {
+    return a+b;
+
+};
+
+const sum2 = (a,b) => a+b;  /!*{
+    return a+b;
+};
+*!/
+*/
+
+const pow = function (number) {
+    return number*number;
+
+};
+
+const  arrowPow = (number) =>{  // стандартный ввид стрелочной функция.
+    return number*number;
+};
+
+
+const getObject2 = () => ({}); //  функция возращает обьект. Стрелочная функция с одним параметром  краткая ззапись.
