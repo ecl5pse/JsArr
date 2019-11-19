0a6579c367e73ca3d513b87794e8b03219d10ff9
ecl5pse <fantamas20@gmail.com>
2019-11-19 20:38:04 +0200

    work  proto

--- js/index.js
+++ js/index.js
@@ -27,14 +27,64 @@ const sum2 = (a,b) => a+b;  /!*{
 *!/
 */
 
+/*
 const pow = function (number) {
     return number*number;
 
 };
-
+`
 const  arrowPow = (number) =>{  // стандартный ввид стрелочной функция.
     return number*number;
 };
 
 
 const getObject2 = () => ({}); //  функция возращает обьект. Стрелочная функция с одним параметром  краткая ззапись.
+*/
+
+
+/*
+const sum = (a , b ) => a+b;
+*/
+
+
+/*
+
+const sum = function (a,b) {
+
+
+    return +a + +b;
+};
+
+*/
+
+//
+// function Person(name , surname , age) {
+//
+//     this.name = name;
+//     thiis
+//
+// }
+//
+
+
+const car1 = {
+    _color: "red",
+
+    _isRepainted: false,
+
+
+    get color(){
+
+        return this._color;
+    },
+
+    set color (value){
+        this._color = value;
+        this._isRepainted = true;
+    },
+
+};
+
+console.log(car1);
+car1.color = "Black";
+console.log(car1);
